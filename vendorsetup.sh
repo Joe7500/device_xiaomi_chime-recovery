0a3a6f55435f67076e1f651b1c6f0a082dabc2b6
Joe7500 <j7545032@gmail.com>
2026-05-26 11:34:35 +0000

    chime: bump maintainer version
    Signed-off-by: Joe7500 <j7545032@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -78,7 +78,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_CLOCK_POS=1
 
 	# R11.3 Settings
-	export FOX_MAINTAINER_PATCH_VERSION="10"
+	export FOX_MAINTAINER_PATCH_VERSION="11"
 	export OF_MAINTAINER="Joe7500"
 	export OF_MAINTAINER_AVATAR="device/xiaomi/chime/icon.png"
 
